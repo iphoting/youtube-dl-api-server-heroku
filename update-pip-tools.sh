59e88bca6f68519dedda638dec4e5d7f292e2b31
Ronald Ip <myself@iphoting.com>
2019-09-13 13:31:12 +0800

    Updated package update instructions.

--- update-pip-tools.sh
+++ update-pip-tools.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # pip install -U virtualenvwrapper
+#mkvirtualenv iphoting-yt-dl-api-p3 -r requirements-dev.txt -i virtualenvwrapper
 #workon iphoting-yt-dl-api
-pip install -U pip-tools
-pip-compile --upgrade
-pip-sync
+pip-compile --upgrade --generate-hashes
+pipenv lock
+#pip-sync
 #deactivate
